472cc5177a82f63655a6fc99e132fa5bbcee6994
Lizeth Castro <lorenacr28@gmail.com>
2023-09-11 10:00:29 -0400

    carga inicial de datos, crear tabla estados, cargar tabla de forma automatica al inicio

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+/**
+ * Author:  Lorena
+ * Created: 11 sep. 2023
+ */
+CREATE TABLE IF NOT EXISTS `estados` (
+  `id_estado` int(11) NOT NULL AUTO_INCREMENT,
+  `estado` varchar(250) NOT NULL,
+  `iso_3166-2` varchar(4) NOT NULL,
+  PRIMARY KEY (`id_estado`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=26 ;
